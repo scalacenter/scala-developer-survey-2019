18b03a184e474486d5bf40488c7c26529671f938
Julien Richard-Foy <julien@richard-foy.fr>
2020-03-03 09:41:10 +0100

    Fix URL prefix

--- render/src/main/scala/survey/Endpoints.scala
+++ render/src/main/scala/survey/Endpoints.scala
@@ -5,7 +5,7 @@ import endpoints.xhr
 object Endpoints extends xhr.future.Endpoints with xhr.JsonEntitiesFromSchemas with FormSummarySchema {
 
   val formResults = endpoint(
-    get(path / "results.json"),
+    get(path / "scala-developer-survey-2019" / "results.json"),
     ok(jsonResponse[FormSummary])
   )
 
